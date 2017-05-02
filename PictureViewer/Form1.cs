8e92c273167d07c7aa2f866d6b3e2cd5662291b0
Mahesh Jethva <mjethvar8@gmail.com>
2017-05-02 09:57:38 -0400

    form changes

--- PictureViewer/Form1.cs
+++ PictureViewer/Form1.cs
@@ -16,5 +16,10 @@ namespace PictureViewer
         {
             InitializeComponent();
         }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
     }
 }
